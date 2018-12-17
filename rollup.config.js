2dd9a52a4c4918344325fd22425fb627987188cf
Rich Lewis <richlewis42@users.noreply.github.com>
2018-12-17 21:56:42 +0000

    build: tweaks to build
    - add minification with rollup-plugin-terser
    - add core-js/babel-runtime to externals
    - change the UMD export to be Callable

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,31 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
+
+const production = !(
+  process.env.NODE_ENV === 'production' && process.env.ROLLUP_WATCH
+)
+
+const args = {
+  input: 'src/index.js',
+  external: id => /^(@babel\/runtime|core-js)/.test(id),
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      runtimeHelpers: true
+    }),
+    production && terser()
+  ]
+}
 
 export default [
   {
     input: 'src/index.js',
     output: {
-      name: 'callable',
+      name: 'Callable',
       file: pkg.browser,
       format: 'umd'
     },
@@ -18,20 +35,16 @@ export default [
         runtimeHelpers: true
       }),
       resolve(),
-      commonjs()
+      commonjs(),
+      production && terser()
     ]
   },
   {
-    input: 'src/index.js',
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
-    ],
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        runtimeHelpers: true
-      })
-    ]
+    output: { file: pkg.module, format: 'es', sourcemap: true },
+    ...args
+  },
+  {
+    output: { file: pkg.main, format: 'cjs', sourcemap: true },
+    ...args
   }
 ]
