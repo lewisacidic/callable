21ec7bee8f857bf23be055d35a1adff91cee03ba
Rich Lewis <richlewis42@users.noreply.github.com>
2018-12-17 16:54:03 +0000

    fix(package): replace boilerplate and upgrade deps
    Replace the starter boilerplate, and upgrade dependencies to latest
    using `yarn upgrade --latest`.

--- rollup.config.js
+++ rollup.config.js
@@ -8,7 +8,7 @@ export default [
   {
     input: 'src/index.js',
     output: {
-      name: 'countdown',
+      name: 'callable',
       file: pkg.browser,
       format: 'umd'
     },
@@ -23,7 +23,6 @@ export default [
   },
   {
     input: 'src/index.js',
-    external: ['lodash'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
