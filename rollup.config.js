7406181145f6d59d3b2afb31abc4145e073f3962
Rich Lewis <richlewis42@users.noreply.github.com>
2019-01-31 17:35:36 +0000

    build(*): update deps and build
    Update rollup-plugin-terser, which fixes the problem with linting
    multiple targets in one step.
    Thus update rollup config to share config for es and cjs builds.

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import babel from 'rollup-plugin-babel'
-import { uglify } from 'rollup-plugin-uglify'
+import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
@@ -9,46 +9,38 @@ const production = !(
   process.env.NODE_ENV === 'production' && process.env.ROLLUP_WATCH
 )
 
-const args = {
-  input: 'src/index.js',
-  output: [
-    { file: pkg.module, format: 'es', sourcemap: true },
-    { file: pkg.main, format: 'cjs', sourcemap: true }
-  ],
-  external: id => /^(@babel\/runtime|core-js)/.test(id),
-  plugins: [
-    babel({
-      exclude: 'node_modules/**',
-      runtimeHelpers: true
-    }),
-    production && uglify()
-  ]
-}
-
 export default [
   {
     input: 'src/index.js',
     output: {
-      name: 'Callable',
+      name: pkg.name,
       file: pkg.browser,
-      format: 'umd'
+      format: 'umd',
+      sourcemap: true
     },
     plugins: [
       babel({
         exclude: 'node_modules/**',
         runtimeHelpers: true
       }),
-      resolve(),
+      resolve({ jsnext: true, main: true, browser: true }),
       commonjs(),
-      production && uglify()
+      production && terser()
     ]
   },
   {
-    output: { file: pkg.module, format: 'es', sourcemap: true },
-    ...args
-  },
-  {
-    output: { file: pkg.main, format: 'cjs', sourcemap: true },
-    ...args
+    input: 'src/index.js',
+    output: [
+      { file: pkg.module, format: 'es', sourcemap: true },
+      { file: pkg.main, format: 'cjs', sourcemap: true }
+    ],
+    external: id => /^(@babel\/runtime|core-js)/.test(id),
+    plugins: [
+      babel({
+        exclude: 'node_modules/**',
+        runtimeHelpers: true
+      }),
+      production && terser()
+    ]
   }
 ]
